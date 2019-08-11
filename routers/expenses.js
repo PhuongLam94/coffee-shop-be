6203f24d63cd8cc743f48f24bb82074259033c99
Phuong Lam <phuonglam@kms-technology.com>
2019-08-11 17:18:40 +0700

    add query to get expense

--- routers/expenses.js
+++ routers/expenses.js
@@ -27,8 +27,39 @@ router.post('/expenses', async (ctx) => {
     httpHelper.handleResultDB(ctx, result, message)
 })
 router.get('/expenses', async (ctx) => {
-    if (ctx.state.user.role === "admin")
-        ctx.body = await ctx.app.expenses.find().toArray()
+    if (ctx.state.user.role === "admin"){
+        var fromDate = parseInt(ctx.request.query.fromDate)
+        var toDate = parseInt(ctx.request.query.toDate)
+        var expenseList, orderList
+        if (!fromDate){
+            expenseList = await ctx.app.expenses.find().sort({date: -1}).toArray()
+            orderList = await ctx.app.orders.find().toArray()
+        } else {
+            var query =  {"$gte": fromDate, "$lte": toDate}
+            console.log(query)
+            expenseList = await ctx.app.expenses.find({
+                date: query
+            }).sort({date: -1}).toArray()
+            orderList =  await ctx.app.orders.find({
+                createdDate: query
+            }).sort({date: -1}).toArray() 
+        }
+        var outcome = 0, income = 0, revenue = 0
+        expenseList.forEach(expense => {
+            if (expense.type === 'in')
+                income += expense.amount
+            else
+                outcome += expense.amount
+        })
+        orderList.forEach(order => revenue+=order.amount)
+        var responseBody = {
+            expenseList: expenseList,
+            outcome: outcome,
+            income: income,
+            revenue: revenue
+        }
+        httpHelper.setResponseBody(ctx, responseBody)
+    }
     else
         httpHelper.setResponseErr(ctx, "Bạn không có quyền xem danh sách chi phí/thu nhập!", 403)
 })
